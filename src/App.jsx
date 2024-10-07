65bf9bf19bf23a1a8d53e4d9c23b103f34c8b802
alhaj sarkar <you@domain.com>
2024-10-07 23:32:21 +0600

    card design

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from 'react'
-import './App.css'
+// import './App.css'
 import Home from './Home/Home';
 import { useEffect } from 'react'
 
@@ -9,28 +9,43 @@ function App() {
   const [product, setProduct] = useState([]);
   const [count, setCount] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch('./data.json')
-    .then(res=> res.json())
-    .then(data =>{
-      setProduct(data)
-    })
+      .then(res => res.json())
+      .then(data => {
+        setProduct(data)
+      })
   }, [])
+
+  const handleCart = (tk) =>{
+    const isNumber = count.find(item => item.id == tk.id);
+    console.log(isNumber)
+    setCount([])
+  }
 
 
 
   // console.log(dolls)
 
   return (
-    <>
-    <div className='grid grid-cols-1 md:grid-cols-6'>
-    {
-        product.map((pd)=>(
-          <Home products={pd} key={pd.id}></Home>
-        ))
-      }
+
+    <div className="grid grid-cols-2 gap-2 m-4 p-4">
+      <div className='grid grid-cols-2 gap-2'>
+        {
+          product.map((pd) => (
+            <Home products={pd} handleCart={handleCart} key={pd.id}></Home>
+          ))
+        }
+      </div>
+      <div className="text-center">
+        <h1 className='text-3xl'>This is Summary</h1>
+        {/* {
+          count.map((Count)=>(
+            console.log(Count)
+          ))
+        } */}
+      </div>
     </div>
-    </>
   )
 }
 
