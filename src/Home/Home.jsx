1c82aac4e60c59e9f27986086313aec135900551
alhaj sarkar <you@domain.com>
2024-10-08 22:01:27 +0600

    card add

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Home = ({ products }) => {
+const Home = ({ products, handleCart }) => {
   // console.log(products, 'hello heros')
   return (
     <div className=''>
@@ -8,14 +8,14 @@ const Home = ({ products }) => {
 
         <figure>
           <img
-            src={products.img}
+            src={(products.img ? 'https://www.walmart.com/ip/Gerber-Baby-Boy-or-Girl-Unisex-White-Short-Sleeve-Cotton-Bodysuit-3-Pack-Sizes-Preemie-24-Months/2275547541?classType=VARIANT&from=/search')}
             alt="Gerber Clothing" />
         </figure>
         <div className="card-body">
           <h2 className="card-title">Taka</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+            <button  className="btn btn-primary">Buy Now</button>
           </div>
         </div>
       </div>
